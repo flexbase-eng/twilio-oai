9de2dbc0863e615c03f30de18037123cb6947557
Robert Dean <robert@flex.one>
2024-05-20 10:56:01 -0400

    Modified 45 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0Njk5NA/spectral.js?branch_slug=main&token=ee0ce41c-af1e-4bec-b9e5-1e6842ed6d9f";
+export default { extends: ruleset };
